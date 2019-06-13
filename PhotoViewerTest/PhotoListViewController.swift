d823be722920070fc8046c025922e9d9abcd9b27
So1fong <zireael6@gmail.com>
2019-06-13 22:32:47 +0300

    first commit

---
+++ PhotoViewerTest/PhotoListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoListViewController.swift
+//  PhotoViewerTest
+//
+//  Created by Kateryna Kozlova on 13/06/2019.
+//  Copyright © 2019 Kateryna Kozlova. All rights reserved.
+//
+
+import UIKit
+
+class PhotoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PhotoViewerTest/Request.swift
@@ -0,0 +1,9 @@
+//
+//  Request.swift
+//  PhotoViewerTest
+//
+//  Created by Kateryna Kozlova on 13/06/2019.
+//  Copyright © 2019 Kateryna Kozlova. All rights reserved.
+//
+
+import Foundation
